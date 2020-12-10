01f279fd0547f9834c4ae49b03aaf2d4648038f4
Sh-Zh-7 <2431297348@qq.com>
2020-12-10 12:30:11 +0800

    docs: update user api document with register validation.

--- sql/blkmkt_user.sql
+++ sql/blkmkt_user.sql
@@ -1,3 +1,6 @@
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
@@ -16,3 +19,52 @@ CREATE TABLE `user`  (
   PRIMARY KEY (`id`)
 );
 ALTER TABLE `user` COMMENT '用户表';
+
+-- ----------------------------
+-- Table structure for permission
+-- ----------------------------
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission`  (
+  `id` int(11) NOT NULL COMMENT '权限id',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限名',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+ALTER TABLE `permission` COMMENT '权限表';
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `id` int(5) NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `role` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色名称',
+  `level` int(5) NULL DEFAULT NULL COMMENT '角色等级超级管理员（3），管理员（2），普通会员（1）',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+ALTER TABLE `role` COMMENT '角色表';
+
+-- ----------------------------
+-- Table structure for role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission`  (
+  `role_id` int(11) NULL DEFAULT NULL COMMENT '角色id',
+  `permission_id` int(11) NOT NULL AUTO_INCREMENT '权限id',
+  INDEX `permisson_id`(`permission_id`) USING BTREE,
+  INDEX `role_id`(`role_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+ALTER TABLE `role_permission` COMMENT '角色权限表';
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role`  (
+  `user_id` int(5) NOT NULL COMMENT '用户id',
+  `role_id` int(5) NULL DEFAULT NULL COMMENT '角色id',
+  PRIMARY KEY (`userid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+ALTER TABLE `user_role` COMMENT '用户角色表';
+
+
+
