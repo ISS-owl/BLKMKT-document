056168b18d7ef2a1437a62f79f78e1b65c3f55fb
Sh-Zh-7 <2431297348@qq.com>
2020-12-12 19:58:20 +0800

    fix: replace name in good model with title.

--- sql/blkmkt_good.sql
+++ sql/blkmkt_good.sql
@@ -1,15 +1,17 @@
 DROP TABLE IF EXISTS `good`;
 CREATE TABLE `good`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '货物id',
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '货物名称',
   `owner_id` int(11) NOT NULL COMMENT '卖家id',
   `price` int(11) NOT NULL COMMENT '价格',
-  `gtype` int(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '出货收货[0 - 出货, 1 - 收货]',
+  `gtype` int(2) NOT NULL COMMENT '出货收货[0 - 出货, 1 - 收货]',
   `category` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '分类',
   `total_num` int(11) NOT NULL COMMENT '总数',
   `current_num` int(11) NOT NULL COMMENT '当前数量',
   `like_num` int(11) COMMENT '点赞数',
   `good_img_url` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '商品图片的url', 
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标题',
+  `subtitle` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '副标题',
+  `description` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '描述',
   `create_time` datetime(0) NOT NULL COMMENT '创建时间',
   `update_time` datetime(0) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
